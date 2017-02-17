a981d1b99164f8e23066dd53134dfc053c140d52
Djib <coudert@novembre.com>
2017-02-17 10:27:41 +0100

    first commit

---
+++ src/class/Test.class.php
@@ -0,0 +1,18 @@
+<?php namespace Novembre;
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author Djb
+*/
+class Test
+{
+    public function debug( $data )
+    {
+        echo '<pre>';
+        var_dump($data);
+        echo '</pre>';
+    }
+}
