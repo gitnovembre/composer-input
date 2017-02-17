0d8f108488e80952ec3b7daa71cca8ba802bad7d
Djib <coudert@novembre.com>
2017-02-17 16:40:23 +0100

    first commit

---
+++ src/Input.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Novembre\Input;
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author Djb
+*/
+
+class Input
+{
+    public function debug( $data )
+    {
+        echo '<pre>';
+        var_dump($data);
+        echo '</pre>';
+    }
+}
