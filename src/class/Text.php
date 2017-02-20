cf72cb68d25cca1b6bae5553c2305a1325c4db8c
Djib <coudert@novembre.com>
2017-02-20 12:09:08 +0100

    developpment

---
+++ src/class/Text.php
@@ -0,0 +1,22 @@
+<?php
+class Text extends Input
+{
+	public function __construct($type, $name="")
+	{
+		$this->name = $name;
+
+		$this->addAttributes(array(
+			'type' => $type,
+			'name' => $name,
+			'role' => "textbox"
+		));
+	}
+	public function value($value)
+	{
+		$this->addAttributes('value', $value);
+		return $this;
+	}
+
+}
+
+?>
